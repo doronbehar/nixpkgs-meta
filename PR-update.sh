6beb39979da947d0c8a7c45f771070abd72d4766
Doron Behar <doron.behar@gmail.com>
2020-04-27 17:13:35 +0300

    Add PR-update script using hub

---
+++ PR-update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if git name-rev HEAD | cut -d' ' -f2 | grep -qv 'update-'; then
+	tput setaf 1
+	echo $0: you are on $(git name-rev HEAD), refusing to open PR >&2
+	tput sgr0
+	exit 1
+fi
+
+hub pull-request \
+	--message "$(git log --oneline -n 1 --format=%B | head -1)" \
+	--message "$(cat standard-PR-templates/single-commit-update.txt)" \
+	--browse \
+	--edit \
+	--push
