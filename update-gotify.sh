b030725d910c43e71926d9b0ccb22585108389ed
Doron Behar <doron.behar@gmail.com>
2023-04-08 15:32:57 +0300

    Fix gotify update script call

--- update-gotify.sh
+++ update-gotify.sh
@@ -2,8 +2,8 @@
 
 git checkout master
 git pull
-git checkout -B pkg/gotify/server/update
-nix-shell maintainers/scripts/update.nix --argstr package pkg/gotify/server/update
+git checkout -B pkg/gotify
+pkgs/servers/gotify/update.sh
 git add pkgs/\*gotify\*
 git commit -m "$(git diff --cached \*version.nix | tail -2 | awk -F\" \
 	'{ prev_ver=$2; getline; next_ver=$2 } END { printf("gotify-server: %s -> %s\n", prev_ver, next_ver) }' \
