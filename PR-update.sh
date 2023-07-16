86c66b396fac30336822ae19a580ec8df7d97507
Doron Behar <doron.behar@gmail.com>
2023-07-16 13:51:18 +0300

    Just open prs in updates - edit them later if needed

--- PR-update.sh
+++ PR-update.sh
@@ -11,5 +11,4 @@ git push -u doronbehar
 
 gh pr create \
 	--title "$(git log --oneline -n 1 --format=%B | head -1)" \
-	--body-file .m/standard-PR-templates/single-commit-update.txt \
-	--web \
+	--body-file .m/standard-PR-templates/single-commit-update.txt
