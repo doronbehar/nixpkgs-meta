53ebee654d29a3ec4e016c3916300c506086ae6e
Doron Behar <doron.behar@gmail.com>
2024-07-07 16:16:42 +0300

    Fix small shellcheck warning in PR-update.sh

--- PR-update.sh
+++ PR-update.sh
@@ -2,7 +2,7 @@
 
 if git name-rev HEAD | cut -d' ' -f2 | grep -qv 'pkg/'; then
 	tput setaf 1
-	echo $0: you are on $(git name-rev HEAD), refusing to open PR >&2
+	echo "$0": you are on "$(git name-rev HEAD)", refusing to open PR >&2
 	tput sgr0
 	exit 1
 fi
