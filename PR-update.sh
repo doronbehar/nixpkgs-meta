cbb83d7ab7a550f9aacd4cc5f2533e9148c3b45c
Doron Behar <doron.behar@gmail.com>
2025-11-17 09:35:24 +0200

    Use Nixpkgs' PR template and replace strings there

--- PR-update.sh
+++ PR-update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if git name-rev HEAD | cut -d' ' -f2 | grep -qvE '(pkg|nixos|doc)/'; then
-	tput setaf 1
-	echo "$0": you are on "$(git name-rev HEAD)", refusing to open PR >&2
-	tput sgr0
-	exit 1
+    tput setaf 1
+    echo "$0": you are on "$(git name-rev HEAD)", refusing to open PR >&2
+    tput sgr0
+    exit 1
 fi
 
 feature_branch="$(git rev-parse --abbrev-ref HEAD)"
@@ -38,15 +38,42 @@ else
     exit 2
 fi
 
+# Written by Claude, inspired by Nixpkgs' substituteInPlace
+replace_literal() {
+    string="$1"
+    search="$2"
+    replace="$3"
+    result=""
+    while case "$string" in *"$search"*) true;; *) false;; esac; do
+        # Get prefix before first match
+        prefix="${string%%"$search"*}"
+        # Append prefix and replacement to result
+        result="$result$prefix$replace"
+        # Remove prefix and search string from input
+        string="${string#*"$search"}"
+    done
+    # Append remaining string
+    result="$result$string"
+    printf '%s\n' "$result"
+}
+
+pr_body="$(cat .github/PULL_REQUEST_TEMPLATE.md)"
+pr_body="$(replace_literal "$pr_body" "[ ] x86_64-linux" "[x] x86_64-linux")"
+pr_body="$(replace_literal \
+  "$pr_body" \
+  "[ ] Tested basic functionality of all binary files" \
+  "[x] Tested basic functionality of all binary files" \
+)"
+pr_body="$(replace_literal "$pr_body" "[ ] Fits [CONTRIBUTING.md]" "[x] Fits [CONTRIBUTING.md]")"
 
 git push -u doronbehar
 
 gh pr create \
-	--title "$(git log -n 1 --oneline --format=%B "$(env \
-		FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
-			--header='Choose a git commit to base PR title upon'
-		" \
-		git flog)" | head -1)" \
-	--body-file .m/standard-PR-templates/single-commit-update.txt \
+    --title "$(git log -n 1 --oneline --format=%B "$(env \
+        FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
+            --header='Choose a git commit to base PR title upon'
+        " \
+        git flog)" | head -1)" \
+    --body "$pr_body" \
     --base "$best_parent" \
-	"$@"
+    "$@"
