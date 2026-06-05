1f56ff5d2f6938e8b177b66a4aa685bcbd74fe06
Doron Behar <doron.behar@gmail.com>
2026-06-05 09:24:32 +0300

    Remove now unused update-tectonic script

--- update-tectonic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git checkout master
-git pull
-git checkout -B pkg/tectonic
-nix-update --commit --version-regex 'tectonic%40(.*)' -- tectonic
-git push -f doronbehar pkg/tectonic
-
-./PR-update.sh
